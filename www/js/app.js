f00f59c3a7eb7688e41766e8be74924160335195
Madi <martha@solutionsresource.com>
2015-02-23 16:55:01 +0800

    added view for employee

--- www/js/app.js
+++ www/js/app.js
@@ -4,9 +4,20 @@
     /* ---------------------------------- Local Variables ---------------------------------- */
     HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
     EmployeeListView.prototype.template = Handlebars.compile($("#employee-list-tpl").html());
+    EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());
     var service = new EmployeeService();
     service.initialize().done(function () {
-        $('body').html(new HomeView(service).render().$el);
+      router.addRoute('', function() {
+          $('body').html(new HomeView(service).render().$el);
+      });
+
+      router.addRoute('employees/:id', function(id) {
+          service.findById(parseInt(id)).done(function(employee) {
+              $('body').html(new EmployeeView(employee).render().$el);
+          });
+      });
+
+      router.start();
     });
 
     /* --------------------------------- Event Registration -------------------------------- */
